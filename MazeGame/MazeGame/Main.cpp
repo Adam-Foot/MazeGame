385f2947f8c021cc1fb0b613ee0e405ad7b12e36
Adam-Foot <adam.foot@students.plymouth.ac.uk>
2019-12-19 15:26:24 +0000

    Added FPS and cube animation of which it's speed depends on the FPS value

--- MazeGame/MazeGame/Main.cpp
+++ MazeGame/MazeGame/Main.cpp
@@ -6,9 +6,11 @@
 
 #define ROWS 60.0		// Defines values for X and Y co-ordinates on maze window
 #define COLUMNS 60.0
+#define FPS 10			// Assigns FPS value
 
 void display();
 void displaySizeChange(int, int);	// Callback function for changing size of screen
+void timer(int);					// Callback function for the timer for character movement
 
 
 void init()
@@ -27,6 +29,7 @@ int main(int argc, char** argv)
 	glutCreateWindow("2D Maze Game - Adam Foot (SOFT356 CW2)");		// Creates GLUT window and defines it's title
 	glutDisplayFunc(display);
 	glutReshapeFunc(displaySizeChange);									// Called when the windows size is changed (through maximisation/minimisation)
+	glutTimerFunc(0, timer, 0);									// Timer function
 	init();
 	glutMainLoop();														// Main GLUT loop
 
@@ -34,10 +37,18 @@ int main(int argc, char** argv)
 }
 
 
+int index = 0;							// Keeps track of position
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);	// Clears colour buffer
 	drawMaze();
+	glRectd(index, 30, index + 2, 32);
+	index++;
+	if (index > 60)
+	{
+		index = 0;
+	}
+	
 	glutSwapBuffers();					// Swap buffers and displays the new frame
 }
 
@@ -48,4 +59,10 @@ void displaySizeChange(int width, int height)
 	glLoadIdentity();
 	glOrtho(0.0, COLUMNS, 0.0, ROWS, -1.0, 1.0);		// Sets the clipping planes and distances for depth clipping
 	glMatrixMode(GL_MODELVIEW);												// Set's matrix back to model_view
+}
+
+void timer(int)
+{
+	glutPostRedisplay();							// Calls display function asap - new frame is displayed every time it's called
+	glutTimerFunc(1000 / FPS, timer, 0);	// Called 10 times every second (10 FPS)
 }
